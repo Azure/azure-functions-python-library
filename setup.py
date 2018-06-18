0b22035b5f941952abf76146de1ee5952e94025a
Elvis Pranskevichus <elvis@magic.io>
2018-06-18 16:48:21 -0400

    Initial import of Azure type definitions from azure-python-worker

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+setup(
+    name='azure-functions',
+    version='1.0.0a1',
+    description='Azure Functions for Python',
+    author='Microsoft Corporation',
+    author_email='azpysdkhelp@microsoft.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Environment :: Web Environment',
+        'Development Status :: 3 - Alpha',
+    ],
+    license='MIT',
+    packages=['azure.functions'],
+    extras_require={
+        'dev': [
+            'flake8~=3.5.0',
+            'mypy',
+            'pytest',
+            'requests==2.*',
+        ]
+    },
+    include_package_data=True,
+    test_suite='tests'
+)
