0b249b628c667ca86953722b78320de2e7c60b99
Elvis Pranskevichus <elvis@magic.io>
2019-04-03 15:10:24 -0400

    Bump version to 1.0.0b4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='azure-functions',
-    version='1.0.0b3',
+    version='1.0.0b4',
     description='Azure Functions for Python',
     author='Microsoft Corporation',
     author_email='azpysdkhelp@microsoft.com',
