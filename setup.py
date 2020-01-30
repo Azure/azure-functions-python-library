a63043a035926ef343069f89142d305affb9dc98
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-01-29 17:41:53 -0800

    Version 1.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='azure-functions',
-    version='1.0.7',
+    version='1.0.8',
     description='Azure Functions for Python',
     author='Microsoft Corporation',
     author_email='azpysdkhelp@microsoft.com',
