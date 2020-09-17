7a31be1ab09d3c9ea33f46f7e7d7283b9f582ccf
David Justo <david.justo.1996@gmail.com>
2020-09-17 16:04:20 -0700

    Added support for Entity Trigger (#68)
    * added durable entities trigger
    
    * fixed style error
    
    * more style issues
    
    * exported entitycontext, generalized tests
    
    * flake8 errors now fixed
    
    Co-authored-by: Varad Meru <vrdmr@users.noreply.github.com>

--- azure/functions/_durable_functions.py
+++ azure/functions/_durable_functions.py
@@ -109,3 +109,31 @@ class OrchestrationContext(_abc.OrchestrationContext):
 
     def __str__(self):
         return self.__body
+
+
+class EntityContext(_abc.OrchestrationContext):
+    """A durable function entity context.
+
+    :param str body:
+        The body of orchestration context json.
+    """
+
+    def __init__(self,
+                 body: Union[str, bytes]) -> None:
+        if isinstance(body, str):
+            self.__body = body
+        if isinstance(body, bytes):
+            self.__body = body.decode('utf-8')
+
+    @property
+    def body(self) -> str:
+        return self.__body
+
+    def __repr__(self):
+        return (
+            f'<azure.EntityContext '
+            f'body={self.body}>'
+        )
+
+    def __str__(self):
+        return self.__body
