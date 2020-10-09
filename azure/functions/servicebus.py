3516a94d67eec3fb0a9ac18962dc6452bd8e3ee0
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-10-09 15:29:52 -0700

    Add cardinality == many

--- azure/functions/servicebus.py
+++ azure/functions/servicebus.py
@@ -2,7 +2,8 @@
 # Licensed under the MIT License.
 
 import datetime
-import typing
+import json
+from typing import Dict, Any, List, Union, Optional, Mapping
 
 from azure.functions import _servicebus as azf_sbus
 
@@ -15,23 +16,23 @@ class ServiceBusMessage(azf_sbus.ServiceBusMessage):
     def __init__(
             self, *,
             body: bytes,
-            trigger_metadata: typing.Mapping[str, typing.Any] = None,
-            content_type: typing.Optional[str] = None,
-            correlation_id: typing.Optional[str] = None,
-            delivery_count: typing.Optional[int] = 0,
-            enqueued_time_utc: typing.Optional[datetime.datetime] = None,
-            expiration_time: typing.Optional[datetime.datetime] = None,
-            expires_at_utc: typing.Optional[datetime.datetime] = None,
-            label: typing.Optional[str] = None,
+            trigger_metadata: Mapping[str, Any] = None,
+            content_type: Optional[str] = None,
+            correlation_id: Optional[str] = None,
+            delivery_count: Optional[int] = 0,
+            enqueued_time_utc: Optional[datetime.datetime] = None,
+            expiration_time: Optional[datetime.datetime] = None,
+            expires_at_utc: Optional[datetime.datetime] = None,
+            label: Optional[str] = None,
             message_id: str,
-            partition_key: typing.Optional[str] = None,
-            reply_to: typing.Optional[str] = None,
-            reply_to_session_id: typing.Optional[str] = None,
-            scheduled_enqueue_time: typing.Optional[datetime.datetime] = None,
-            session_id: typing.Optional[str] = None,
-            time_to_live: typing.Optional[datetime.timedelta] = None,
-            to: typing.Optional[str] = None,
-            user_properties: typing.Dict[str, object]) -> None:
+            partition_key: Optional[str] = None,
+            reply_to: Optional[str] = None,
+            reply_to_session_id: Optional[str] = None,
+            scheduled_enqueue_time: Optional[datetime.datetime] = None,
+            session_id: Optional[str] = None,
+            time_to_live: Optional[datetime.timedelta] = None,
+            to: Optional[str] = None,
+            user_properties: Dict[str, object]) -> None:
 
         self.__body = body
         self.__trigger_metadata = trigger_metadata
@@ -53,38 +54,38 @@ class ServiceBusMessage(azf_sbus.ServiceBusMessage):
         self.__user_properties = user_properties
 
         # Cache for trigger metadata after Python object conversion
-        self._trigger_metadata_pyobj: typing.Optional[
-            typing.Mapping[str, typing.Any]] = None
+        self._trigger_metadata_pyobj: Optional[
+            Mapping[str, Any]] = None
 
     def get_body(self) -> bytes:
         return self.__body
 
     @property
-    def content_type(self) -> typing.Optional[str]:
+    def content_type(self) -> Optional[str]:
         return self.__content_type
 
     @property
-    def correlation_id(self) -> typing.Optional[str]:
+    def correlation_id(self) -> Optional[str]:
         return self.__correlation_id
 
     @property
-    def delivery_count(self) -> typing.Optional[int]:
+    def delivery_count(self) -> Optional[int]:
         return self.__delivery_count
 
     @property
-    def enqueued_time_utc(self) -> typing.Optional[datetime.datetime]:
+    def enqueued_time_utc(self) -> Optional[datetime.datetime]:
         return self.__enqueued_time_utc
 
     @property
-    def expiration_time(self) -> typing.Optional[datetime.datetime]:
+    def expiration_time(self) -> Optional[datetime.datetime]:
         return self.__expiration_time
 
     @property
-    def expires_at_utc(self) -> typing.Optional[datetime.datetime]:
+    def expires_at_utc(self) -> Optional[datetime.datetime]:
         return self.__expires_at_utc
 
     @property
-    def label(self) -> typing.Optional[str]:
+    def label(self) -> Optional[str]:
         return self.__label
 
     @property
@@ -92,39 +93,39 @@ class ServiceBusMessage(azf_sbus.ServiceBusMessage):
         return self.__message_id
 
     @property
-    def partition_key(self) -> typing.Optional[str]:
+    def partition_key(self) -> Optional[str]:
         return self.__partition_key
 
     @property
-    def reply_to(self) -> typing.Optional[str]:
+    def reply_to(self) -> Optional[str]:
         return self.__reply_to
 
     @property
-    def reply_to_session_id(self) -> typing.Optional[str]:
+    def reply_to_session_id(self) -> Optional[str]:
         return self.__reply_to_session_id
 
     @property
-    def scheduled_enqueue_time(self) -> typing.Optional[datetime.datetime]:
+    def scheduled_enqueue_time(self) -> Optional[datetime.datetime]:
         return self.__scheduled_enqueue_time
 
     @property
-    def session_id(self) -> typing.Optional[str]:
+    def session_id(self) -> Optional[str]:
         return self.__session_id
 
     @property
-    def time_to_live(self) -> typing.Optional[datetime.timedelta]:
+    def time_to_live(self) -> Optional[datetime.timedelta]:
         return self.__time_to_live
 
     @property
-    def to(self) -> typing.Optional[str]:
+    def to(self) -> Optional[str]:
         return self.__to
 
     @property
-    def user_properties(self) -> typing.Dict[str, object]:
+    def user_properties(self) -> Dict[str, object]:
         return self.__user_properties
 
     @property
-    def metadata(self) -> typing.Optional[typing.Mapping[str, typing.Any]]:
+    def metadata(self) -> Optional[Mapping[str, Any]]:
         """Getting read-only trigger metadata in a Python dictionary.
 
         Exposing the raw trigger_metadata to our customer. For cardinality=many
@@ -135,7 +136,7 @@ class ServiceBusMessage(azf_sbus.ServiceBusMessage):
 
         Returns:
         --------
-        typing.Mapping[str, object]
+        Mapping[str, object]
             Return the Python dictionary of trigger metadata
         """
         if self.__trigger_metadata is None:
@@ -165,9 +166,40 @@ class ServiceBusMessageInConverter(meta.InConverter,
         return issubclass(pytype, azf_sbus.ServiceBusMessage)
 
     @classmethod
-    def decode(cls, data: meta.Datum, *,
-               trigger_metadata) -> ServiceBusMessage:
+    def decode(
+        cls, data: meta.Datum, *, trigger_metadata: Mapping[str, meta.Datum]
+    ) -> Union[ServiceBusMessage, List[ServiceBusMessage]]:
+        """Returns the application setting from environment variable.
 
+        Parameters
+        ----------
+        data: meta.Datum
+            The datum from GRPC message
+
+        trigger_metadata: Mapping[str, meta.Datum]
+            The metadata of the Service Bus trigger, usually populated by
+            function host
+
+        Returns
+        -------
+        Union[ServiceBusMessage, List[ServiceBusMessage]]
+            When 'cardinality' is set to 'one', this method returns a single
+            ServiceBusMessage. When 'cardinality' is set to 'many' this method
+            returns a list of ServiceBusMessage.
+        """
+        if cls._is_cardinality_one(trigger_metadata):
+            raise Exception('DECODE SINGLE MESSAGE')
+            return cls.decode_single_message(data, trigger_metadata)
+        elif cls._is_cardinality_many(trigger_metadata):
+            raise Exception('DECODE MULTIPLE MESSAGE')
+            return cls.decode_multiple_messages(data, trigger_metadata)
+        else:
+            raise NotImplementedError(
+                f'unsupported service bus data type: {data.type}')
+
+    @classmethod
+    def decode_single_message(cls, data: meta.Datum, *,
+        trigger_metadata: Mapping[str, meta.Datum]) -> ServiceBusMessage:
         if data is None:
             # ServiceBus message with no payload are possible.
             # See Azure/azure-functions-python-worker#330
@@ -224,6 +256,54 @@ class ServiceBusMessageInConverter(meta.InConverter,
                 trigger_metadata, 'UserProperties', python_type=dict),
         )
 
+    @classmethod
+    def decode_multiple_messages(cls, data: meta.Datum, *,
+        trigger_metadata: Mapping[str, meta.Datum]) -> List[ServiceBusMessage]:
+        if data.type == 'collection_bytes':
+            parsed_data = data.value.bytes
+
+        elif data.type == 'collection_string':
+            parsed_data = data.value.string
+
+        elif data.type == 'json':
+            parsed_data = json.loads(data.value)
+
+        sys_props = trigger_metadata.get('SystemPropertiesArray')
+
+        parsed_sys_props: List[Any] = []
+        if sys_props is not None:
+            parsed_sys_props = json.loads(sys_props.value)
+
+        messages = []
+        for i in range(len(parsed_data)):
+            enqueued_time = parsed_sys_props[i].get('EnqueuedTimeUtc')
+
+            message = ServiceBusMessage(
+                body=cls._marshall_message_body(parsed_data[i], data.type),
+                trigger_metadata=trigger_metadata,
+            )
+
+            messages.append(message)
+
+        return messages
+
+    @classmethod
+    def _is_cardinality_many(cls, trigger_metadata) -> bool:
+        return 'SystemPropertiesArray' in trigger_metadata
+
+    @classmethod
+    def _is_cardinality_one(cls, trigger_metadata) -> bool:
+        return 'SystemProperties' in trigger_metadata
+
+    @classmethod
+    def _marshall_message_body(cls, parsed_data, data_type) -> str:
+        if data_type == 'bytes':
+            return parsed_data.encode('utf-8')
+
+        if data_type == 'json':
+            return json.dumps(parsed_data).encode('utf-8')
+
+        return parsed_data
 
 class ServiceBusMessageOutConverter(meta.OutConverter, binding='serviceBus'):
 
@@ -232,8 +312,8 @@ class ServiceBusMessageOutConverter(meta.OutConverter, binding='serviceBus'):
         return issubclass(pytype, (str, bytes))
 
     @classmethod
-    def encode(cls, obj: typing.Any, *,
-               expected_type: typing.Optional[type]) -> meta.Datum:
+    def encode(cls, obj: Any, *,
+               expected_type: Optional[type]) -> meta.Datum:
         if isinstance(obj, str):
             return meta.Datum(type='string', value=obj)
 
