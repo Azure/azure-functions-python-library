8bc68fd204d2ade2ffa0ec9c18a655b074cd64e0
David Justo <david.justo.1996@gmail.com>
2020-11-20 11:07:12 -0800

    Remove typing from namespace extension for Docs CI compliance (#76)
    * remove typing from namespace extension for Docs CI compliance

--- azure/__init__.py
+++ azure/__init__.py
@@ -1,7 +1,4 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from pkgutil import extend_path
-import typing
-
-__path__: typing.Iterable[str] = extend_path(__path__, __name__)
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
