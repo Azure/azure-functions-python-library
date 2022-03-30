e1b4780666bace13ab267ee5bb1fb3274cb89a08
pdthummar <pthummar@microsoft.com>
2022-03-30 15:35:52 -0500

    updated the file as per the recommendation.

--- azure/functions/timer.py
+++ azure/functions/timer.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 import json
 import typing
 
@@ -7,22 +10,22 @@ from . import meta
 
 class TimerRequest(azf_abc.TimerRequest):
 
-    def __init__(self, *, past_due: bool, schedulestatus: dict,
-                 schedule: dict) -> None:
+    def __init__(self, *, past_due: bool = False, schedule_status: dict = {},
+                 schedule: dict = {}) -> None:
         self.__past_due = past_due
-        self.__schedulestatus = schedulestatus
+        self.__schedule_status = schedule_status
         self.__schedule = schedule
 
     @property
-    def past_due(self):
+    def past_due(self) -> bool:
         return self.__past_due
 
     @property
-    def schedulestatus(self):
-        return self.__schedulestatus
+    def schedule_status(self) -> dict:
+        return self.__schedule_status
 
     @property
-    def schedule(self):
+    def schedule(self) -> dict:
         return self.__schedule
 
 
@@ -42,5 +45,5 @@ class TimerRequestConverter(meta.InConverter,
 
         return TimerRequest(
             past_due=info.get('IsPastDue', False),
-            schedulestatus=info["ScheduleStatus"],
-            schedule=info["Schedule"])
+            schedule_status=info.get('ScheduleStatus', {}),
+            schedule=info.get('Schedule', {}))
