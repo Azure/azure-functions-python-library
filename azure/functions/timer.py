d0d3f8fb520d84c7d6a55be54e5ed952a891120c
pdthummar <101662222+pdthummar@users.noreply.github.com>
2022-04-05 15:03:38 -0500

    Add missing fields (ScheduleStatus) in timer object. (#110)

--- azure/functions/timer.py
+++ azure/functions/timer.py
@@ -5,18 +5,28 @@ import json
 import typing
 
 from azure.functions import _abc as azf_abc
-
 from . import meta
 
 
 class TimerRequest(azf_abc.TimerRequest):
 
-    def __init__(self, *, past_due: bool) -> None:
+    def __init__(self, *, past_due: bool = False, schedule_status: dict = {},
+                 schedule: dict = {}) -> None:
         self.__past_due = past_due
+        self.__schedule_status = schedule_status
+        self.__schedule = schedule
 
     @property
-    def past_due(self):
+    def past_due(self) -> bool:
         return self.__past_due
+
+    @property
+    def schedule_status(self) -> dict:
+        return self.__schedule_status
+
+    @property
+    def schedule(self) -> dict:
+        return self.__schedule
 
 
 class TimerRequestConverter(meta.InConverter,
@@ -32,5 +42,8 @@ class TimerRequestConverter(meta.InConverter,
             raise NotImplementedError
 
         info = json.loads(data.value)
+
         return TimerRequest(
-            past_due=info.get('IsPastDue', False))
+            past_due=info.get('IsPastDue', False),
+            schedule_status=info.get('ScheduleStatus', {}),
+            schedule=info.get('Schedule', {}))
