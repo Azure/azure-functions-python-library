f5e77c7707df1e0237290d227fac47bf59f3cbf6
peterstone2017 <yunchuwang5@gmail.com>
2022-05-11 14:32:35 -0500

    Revert "Add SQL Binding support (#124)"
    This reverts commit 6f10a445ea7019cd146de7e437aeeada7e48a8e5.

--- azure/functions/__init__.py
+++ azure/functions/__init__.py
@@ -17,7 +17,6 @@ from .kafka import KafkaEvent, KafkaConverter, KafkaTriggerConverter
 from .meta import get_binding_registry
 from ._queue import QueueMessage
 from ._servicebus import ServiceBusMessage
-from ._sql import SqlRow, SqlRowList
 
 # Import binding implementations to register them
 from . import blob  # NoQA
@@ -30,7 +29,6 @@ from . import queue  # NoQA
 from . import servicebus  # NoQA
 from . import timer  # NoQA
 from . import durable_functions  # NoQA
-from . import sql # NoQA
 
 
 __all__ = (
@@ -57,8 +55,6 @@ __all__ = (
     'EntityContext',
     'QueueMessage',
     'ServiceBusMessage',
-    'SqlRow',
-    'SqlRowList',
     'TimerRequest',
 
     # Middlewares
