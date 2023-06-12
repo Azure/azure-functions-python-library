f2614e4a0e0626516dc187d5d7aeb14eb7323618
pthummar <pthummar@microsoft.com>
2023-06-12 17:05:43 -0500

    testing label creation

--- azure/functions/__init__.py
+++ azure/functions/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+# Testing label creation
 
 from ._abc import TimerRequest, InputStream, Context, Out
 from ._eventhub import EventHubEvent
