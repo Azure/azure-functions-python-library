b14b1c8c985590b85e18918008e43cb09bb1ff1d
Gavin Aguiar <gavin@GavinPC>
2023-06-16 14:16:26 -0500

    Removing retry options from timer trigger

--- azure/functions/decorators/timer.py
+++ azure/functions/decorators/timer.py
@@ -16,20 +16,10 @@ class TimerTrigger(Trigger):
                  schedule: str,
                  run_on_startup: Optional[bool] = None,
                  use_monitor: Optional[bool] = None,
-                 retry_strategy: Optional[str] = None,
-                 retry_max_retry_count: Optional[int] = None,
-                 retry_delay_interval: Optional[str] = None,
-                 retry_minimum_interval: Optional[str] = None,
-                 retry_maximum_interval: Optional[str] = None,
                  data_type: Optional[DataType] = None,
                  **kwargs) -> None:
         self.schedule = schedule
         self.run_on_startup = run_on_startup
         self.use_monitor = use_monitor
-        self.retry_strategy = retry_strategy
-        self.retry_max_retry_count = retry_max_retry_count
-        self.retry_delay_interval = retry_delay_interval
-        self.retry_minimum_interval = retry_minimum_interval
-        self.retry_maximum_interval = retry_maximum_interval
 
         super().__init__(name=name, data_type=data_type)
