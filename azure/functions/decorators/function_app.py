ff2e803e5e93cad115b1d01aaf469aef33966c35
wangbill <12449837+YunchuWang@users.noreply.github.com>
2024-07-17 09:56:42 -0700

    fix asgi/wsgifunctionapps to extend bindingapi/settingapi (#209)
    Co-authored-by: gavin-aguiar <80794152+gavin-aguiar@users.noreply.github.com>
    Co-authored-by: hallvictoria <59299039+hallvictoria@users.noreply.github.com>

--- azure/functions/decorators/function_app.py
+++ azure/functions/decorators/function_app.py
@@ -3313,7 +3313,13 @@ class Blueprint(TriggerApi, BindingApi, SettingsApi):
     pass
 
 
-class ExternalHttpFunctionApp(FunctionRegister, TriggerApi, ABC):
+class ExternalHttpFunctionApp(
+    FunctionRegister,
+    TriggerApi,
+    SettingsApi,
+    BindingApi,
+    ABC
+):
     """Interface to extend for building third party http function apps."""
 
     @abc.abstractmethod
